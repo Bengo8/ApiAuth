e578dadd11119917ffc9f899da046cbbabc1a92f
abengoechea <aitorbengo88@gmail.com>
2022-05-21 17:54:47 +0200

    cambioss

---
+++ authServer.js
@@ -0,0 +1,44 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const jwt = require('jsonwebtoken');
+
+app.use(express.json());
+
+let refreshTokens = [];
+
+app.post('/token', (req, res) => {
+    const refreshToken = req.body.token;
+    if (refreshToken === null) return res.sendStatus(401);
+    if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403);
+
+    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+        if (err) return res.sendStatus(403);
+
+        const accesToken = generateAccessToken({ name: user.name });
+        res.json({ accesToken: accesToken });
+    });
+});
+
+app.delete('/logout', (req, res) => {
+    refreshTokens = refreshTokens.filter(token => token !== req.body.token);
+    res.sendStatus(204);
+})
+
+app.post('/login', (req, res) => {
+
+    
+
+    const user = { origin: "http://localhost:3000/", ipOrigin: "192.168.0.21", secretKey: "1234" };
+
+    const acessToken = generateAccessToken(user);
+    const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET);
+    refreshTokens.push(refreshToken);
+    res.json({ acessToken: acessToken, refreshToken: refreshToken });
+});
+
+function generateAccessToken(user) {
+    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1500s' });
+}
+
+app.listen(5000);
